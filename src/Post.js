083196183dad6e1a8c65cb333354a91012c82d92
Eddy Chase <eddykanyz.ek@gmail.com>
2021-05-20 09:16:09 +0300

    added the avatar

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Post.css';
 
-function Post() {
+function Post({ name, description, message, photoUrl }) {
     return (
         <div>
             <div className="post">
+                <div className="post_header">
+                    <Avatar />
+                </div>
+
+                .
 
             </div>
         </div>
