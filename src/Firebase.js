8678a9d6b46488020165285ea4aa18a793da0dc0
Eddy Chase <eddykanyz.ek@gmail.com>
2021-11-11 20:22:04 +0300

    deploy

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,15 +2,13 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCYN3Lx4J_gAd8wdjUJPF70SXCzocFUHGs",
-    authDomain: "linkedin-clone-5577d.firebaseapp.com",
-    projectId: "linkedin-clone-5577d",
-    storageBucket: "linkedin-clone-5577d.appspot.com",
-    messagingSenderId: "122117143176",
-    appId: "1:122117143176:web:1ed759cfe4154f72eabd3d",
-    measurementId: "G-JMSFJP2KXE"
+  apiKey: "AIzaSyB5wDWAptgZUam9sJpCTl2jxqt4w-vNZJc",
+  authDomain: "social-media-90211.firebaseapp.com",
+  projectId: "social-media-90211",
+  storageBucket: "social-media-90211.appspot.com",
+  messagingSenderId: "976911858646",
+  appId: "1:976911858646:web:d8f2abebfaa76e3ee78277"
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
